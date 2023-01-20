049917847f54b9fb61b72614d7303717aa1d0aa0
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-01-20 14:43:55 +0330

    increase version

--- custom_setup.sh
+++ custom_setup.sh
@@ -6,13 +6,14 @@ VERSION=0.0.2
 
 RELEASE_VERSION=${VERSION}
 
-SDK_BIN_URL=https://github.com/HoseinHaqiqian/webrtc-sdk/releases/download/${RELEASE_VERSION}/libwebrtc.aar
+SDK_BIN_URL=https://github.com/HoseinHaqiqian/webrtc-sdk/releases/download/${VERSION}/libwebrtc.aar
 
 BUILD_DIR=build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 echo "Downloading webrtc-sdk ${VERSION} binary for android."
+echo "Downloading ${SDK_BIN_URL}"
 curl -L -O ${SDK_BIN_URL}
 
 mvn install:install-file \
